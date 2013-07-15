24fe527af4970995b6d849dc88cd440dbcb21285
Sandeep Shekhawat <sandeep.shekhawat@gmail.com>
2013-07-15 07:03:35 +0000

    reading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2222!');
+  var buff = fs.readFileSync('index.html');
+  response.send(buff.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
